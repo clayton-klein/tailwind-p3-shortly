80c45394ba523e0238931d248510ffb1b542e437
clayton-klein <csklein87@hotmail.com>
2023-05-13 11:52:40 -0300

    feat: add a simple validation to the form input.

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const linkForm = document.getElementById("link-form");
+const linkInput = document.getElementById("link-input");
+const alertMsg = document.getElementById("alert-msg");
+
+linkForm.addEventListener("submit", formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault();
+
+  if (!linkInput.value) {
+    linkInput.classList.add("border-red");
+    alertMsg.innerText = "Please enter a URL.";
+    return;
+  } else if (!validURL(linkInput.value)) {
+    linkInput.classList.add("border-red");
+    alertMsg.innerText = "Please enter a valid URL.";
+    return;
+  } else {
+    linkInput.classList.remove("border-red");
+    linkInput.value = "";
+    alertMsg.innerText = "";
+    alert("Success!");
+  }
+}
+
+// Validate a URL
+function validURL(str) {
+  const pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+      "((\\d{1,3}\\.){3}\\d{1,3}))" +
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+      "(\\?[;&a-z\\d%_.~+=-]*)?" +
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return !!pattern.test(str);
+}
